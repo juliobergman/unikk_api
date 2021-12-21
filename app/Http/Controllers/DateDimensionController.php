fa8ee3fb5420b3e053800c6e13552c20887a1ad2
Julio Bergman <me@juliobergman.com>
2021-12-21 12:15:20 -0400

    Date Dimension

---
+++ app/Http/Controllers/DateDimensionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DateDimension;
+use Illuminate\Http\Request;
+
+class DateDimensionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DateDimension  $dateDimension
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DateDimension $dateDimension)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DateDimension  $dateDimension
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DateDimension $dateDimension)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DateDimension  $dateDimension
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DateDimension $dateDimension)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DateDimension  $dateDimension
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DateDimension $dateDimension)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_12_21_161222_create_date_dimensions_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDateDimensionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('date_dimensions', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->unsignedInteger('day');
+            $table->unsignedInteger('month');
+            $table->unsignedInteger('year');
+            $table->string('day_name');
+            $table->string('day_suffix', 2);
+            $table->unsignedInteger('day_of_week');
+            $table->unsignedInteger('day_of_year');
+            $table->unsignedInteger('is_weekend');
+            $table->unsignedInteger('week');
+            $table->unsignedInteger('iso_week');
+            $table->unsignedInteger('week_of_month');
+            $table->unsignedInteger('week_of_year');
+            $table->unsignedInteger('iso_week_in_year');
+            $table->string('month_name');
+            $table->string('month_name_short');
+            $table->string('month_year');
+            $table->string('month_name_year');
+            $table->date('first_day_of_month');
+            $table->date('last_day_of_month');
+            $table->date('first_day_of_next_month');
+            $table->unsignedInteger('quarter');
+            $table->string('quarter_name');
+            $table->date('first_day_of_quarter');
+            $table->date('last_day_of_quarter');
+            $table->date('first_day_of_year');
+            $table->date('last_day_of_year');
+            $table->date('first_day_of_next_year');
+            $table->unsignedInteger('dow_in_month');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('date_dimensions');
+    }
+}
