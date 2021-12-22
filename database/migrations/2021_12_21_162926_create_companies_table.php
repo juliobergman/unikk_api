c25190255e16eebdcba69c5829cedec7048bffe4
Julio Bergman <me@juliobergman.com>
2021-12-22 08:45:34 -0400

    Company Default Currency USD Dollars

--- database/migrations/2021_12_21_162926_create_companies_table.php
+++ database/migrations/2021_12_21_162926_create_companies_table.php
@@ -18,7 +18,8 @@ class CreateCompaniesTable extends Migration
             $table->foreignId('user_id')->constrained();
             $table->unsignedBigInteger('company_id')->nullable();
             $table->foreign('company_id')->references('id')->on('companies');
-            $table->unsignedBigInteger('currency_id')->nullable();
+            $table->unsignedBigInteger('currency_id')->default(2);
+            $table->foreign('currency_id')->references('id')->on('currencies');
             $table->string('name');
             $table->enum('type', ['active','target'])->default('active');
             $table->softDeletes();
