0bb3beb2810efe40dc07532f8ce79921f33611f6
Julio Bergman <me@juliobergman.com>
2021-12-22 08:53:23 -0400

    Company Settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             CompanySeeder::class,
             CompanyDataSeeder::class,
             CompanyTargetDataSeeder::class,
+            CompanySettingsSeeder::class,
         ]);
     }
 }
